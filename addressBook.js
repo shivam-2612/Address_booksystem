2003bf700ae8612fb419429f5dac481e3e588c38
Shivam Goyal <goyal2003shivam@gmail.com>
2025-03-19 19:57:35 +0530

    [Shivam]: UC_2 Completed

--- addressBook.js
+++ addressBook.js
@@ -4,6 +4,7 @@ class AddressBook {
     }
 
     addContact(contact) {
+        if (!this.validateContact(contact)) throw new Error("Invalid contact details!");
         this.contacts.push(contact);
         console.log(`Added: ${contact.firstName} ${contact.lastName}`);
     }
@@ -17,6 +18,24 @@ class AddressBook {
         console.log("Address Book:");
         this.contacts.forEach(c => console.log(`${c.firstName} ${c.lastName} - ${c.email}`));
     }
+
+    validateContact(contact) {
+        const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+        const addressPattern = /^.{4,}$/;
+        const zipPattern = /^\d{5,6}$/;
+        const phonePattern = /^\d{10}$/;
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+        return namePattern.test(contact.firstName) &&
+            namePattern.test(contact.lastName) &&
+            addressPattern.test(contact.address) &&
+            addressPattern.test(contact.city) &&
+            addressPattern.test(contact.state) &&
+            zipPattern.test(contact.zip) &&
+            phonePattern.test(contact.phoneNumber) &&
+            emailPattern.test(contact.email);
+    }
+    
 }
 
 class Contact {
@@ -33,9 +52,14 @@ class Contact {
 }
 
 const myBook = new AddressBook();
+try{
 myBook.addContact(new Contact("Shivam", "Goyal", "2612", "bata chowk", "faridabad", "258796", "788298008", "shivam@gmail.com"));
-myBook.addContact(new Contact("Prince", "Sharma", "7895", "GK", "delhi", "110043", "9149293577", "sharmaji@gmail.com"));
+myBook.addContact(new Contact("Prince", "Sharma", "7895", "GK", "delhi", "110043", "9149293577", "sharmajigmail.com"));
 
 myBook.listContacts();
 myBook.removeContact("shivam@gmail.com");
 myBook.listContacts();
+}
+catch(error){
+    console.log(error.message);
+}
